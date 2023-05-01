8250a38c3dd69eb8bb99efc3981376178edffdb1
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-01 13:55:03 +0530

    update document file

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
-        <Main />
+        <Main></Main>
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
