0f3c4da8b96b29ded138c7dc0fcd7248f283bf8b
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-16 15:32:51 +0530

    minor update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Banner from "@/components/Banner/Banner";
+import Services from "@/components/Services/Services";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <Banner />
+        <Services />
       </div>
     </>
   );
