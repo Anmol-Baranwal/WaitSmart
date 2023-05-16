e9e887f1678095052a97a848c565d4418eb2c8e9
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-16 23:23:33 +0530

    add logo component for future usecase

---
+++ components/Logo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link as ChakraLink, Image } from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function Logo({ image }) {
+  const iconPath = `/static/icons/${image}.png`;
+  return (
+    <Link href="/" passHref>
+      <ChakraLink
+        as="a"
+        style={{ variant: "links.logo", display: "flex", alignItems: "center" }}
+      >
+        <Image src={iconPath} alt="wait smart logo" />
+      </ChakraLink>
+    </Link>
+  );
+}
