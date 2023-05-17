f2544a63c4844344846b0a9dd77adb7b5370d6ab
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-17 22:40:06 +0530

    add card component

---
+++ components/Card/Card.js
@@ -0,0 +1,26 @@
+import { Box, Image, Heading, Text } from "@chakra-ui/react";
+import styles from "./card.module.css";
+import Link from "next/link";
+
+const Card = ({ src, altText, title, designation, social }) => {
+  console.log({ social });
+
+  return (
+    <Box className={styles.card}>
+      <Image src={src} alt={altText} className={styles.memberThumb} />
+      <Box className={styles.infoWrapper}>
+        <Heading className={styles.name}>{title}</Heading>
+        <Text className={styles.designation}>{designation}</Text>
+      </Box>
+      <Box className={styles.socialShare}>
+        {social.map((item) => (
+          <Link key={item.id} href={item.path} className={styles.socialLink}>
+            {item.icon}
+          </Link>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default Card;
