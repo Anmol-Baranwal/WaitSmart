1af2aaac5431680067cfe2d6a8bc7e5fad0f04ee
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-18 15:26:22 +0530

    add workflow with complete responsive styling

--- components/Workflow/workflow.js
+++ components/Workflow/workflow.js
@@ -6,23 +6,23 @@ import styles from "./workflow.module.css";
 import data from "@/data/workflow.json"; // Update the path to the data file
 
 const WorkFlow = () => {
-  //   const renderArrow = (index) => {
-  //     if (index % 2 === 0) {
-  //       return (
-  //         <div
-  //           className={styles.arrow}
-  //           style={{ backgroundImage: `url(${ArrowEven})` }}
-  //         />
-  //       );
-  //     } else {
-  //       return (
-  //         <div
-  //           className={styles.arrow}
-  //           style={{ backgroundImage: `url(${ArrowOdd})`, top: "17px" }}
-  //         />
-  //       );
-  //     }
-  //   };
+  // const renderArrow = (index) => {
+  //   if (index % 2 === 0) {
+  //     return (
+  //       <div
+  //         className={styles.arrow}
+  //         style={{ backgroundImage: `url(${ArrowEven})` }}
+  //       />
+  //     );
+  //   } else {
+  //     return (
+  //       <div
+  //         className={styles.arrow}
+  //         style={{ backgroundImage: `url(${ArrowOdd})`, top: "17px" }}
+  //       />
+  //     );
+  //   }
+  // };
 
   return (
     <section className={styles.workflow}>
@@ -38,8 +38,8 @@ const WorkFlow = () => {
             <Box className={styles.card} key={item.id}>
               <Box className={styles.iconBox}>{`0${item.id}`}</Box>
               <Box className={styles.wrapper}>
-                <Heading className={styles.wrapperTitle}>{item.title}</Heading>
-                <Text className={styles.wrapperSubtitle}>{item.text}</Text>
+                <Heading className={styles.title}>{item.title}</Heading>
+                <Text className={styles.subTitle}>{item.text}</Text>
               </Box>
               {/* {renderArrow(index + 1)} */}
             </Box>
