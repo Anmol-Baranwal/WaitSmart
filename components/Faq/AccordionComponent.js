1c770d905868cde072bc34db3bf253ec7ccd0cc2
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-19 01:59:09 +0530

    add accordian component for faq

---
+++ components/Faq/AccordionComponent.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import styles from "./accordionComponent.module.css";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+
+const AccordionItem = ({ title, contents, isOpen, onClick }) => {
+  return (
+    <div
+      className={`${styles.accordionWrapper} ${isOpen ? styles.open : ""}`}
+      onClick={onClick}
+    >
+      <div className={styles.accordionItem}>
+        <h2 className={styles.title}>{title}</h2>
+        {isOpen ? (
+          <FaChevronUp className={styles.accordionIcon} />
+        ) : (
+          <FaChevronDown className={styles.accordionIcon} />
+        )}
+      </div>
+      {isOpen && <div className={styles.accordionContent}>{contents}</div>}
+    </div>
+  );
+};
+
+const Accordion = ({ data }) => {
+  const [openIndex, setOpenIndex] = useState(0);
+
+  const handleItemClick = (index) => {
+    if (openIndex === index) {
+      // Avoid the closing of the only open option
+      return;
+    }
+
+    setOpenIndex(index);
+    //   setOpenIndex(index === openIndex ? -1 : index);
+  };
+
+  return (
+    <div>
+      {data.map((item, index) => (
+        <AccordionItem
+          key={item.id}
+          title={item.title}
+          contents={item.contents}
+          isOpen={index === openIndex}
+          onClick={() => handleItemClick(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
+
+// ------------- Using Chakra-UI -------------
+// import styles from "./accordionComponent.module.css";
+// import {
+//   Box,
+//   Accordion,
+//   AccordionButton,
+//   AccordionIcon,
+//   AccordionItem,
+//   AccordionPanel,
+// } from "@chakra-ui/react";
+
+// const AccordionComponent = ({ data }) => {
+//   return (
+//     <Accordion className={styles.accordionWrapper} allowToggle>
+//       {data.map((item) => (
+//         <AccordionItem className={styles.accordionItem} key={item.id}>
+//           <h2>
+//             <AccordionButton className={styles.accordionButton}>
+//               <Box className={styles.title} as="span" flex="1" textAlign="left">
+//                 {item.title}
+//               </Box>
+//               <AccordionIcon className={styles.accordionIcon} />
+//             </AccordionButton>
+//           </h2>
+//           <AccordionPanel className={styles.accordionContent}>
+//             {item.contents}
+//           </AccordionPanel>
+//         </AccordionItem>
+//       ))}
+//     </Accordion>
+//   );
+// };
+
+// export default AccordionComponent;
