dc706a3eeef633fff7b0fc3e944c0716860b77de
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-18 11:39:39 +0530

    add more social icons

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,4 +1,4 @@
-import { Box, Image, Heading, Text } from "@chakra-ui/react";
+import { Box, Image, Heading, Text, Container } from "@chakra-ui/react";
 import styles from "./card.module.css";
 import Link from "next/link";
 
@@ -6,20 +6,27 @@ const Card = ({ src, altText, title, designation, social }) => {
   // console.log({ social });
 
   return (
-    <Box className={styles.card}>
-      <Image src={src} alt={altText} className={styles.memberThumb} />
-      <Box className={styles.infoWrapper}>
-        <Heading className={styles.name}>{title}</Heading>
-        <Text className={styles.designation}>{designation}</Text>
+    <Container className={styles.container}>
+      <Box className={styles.card}>
+        <Image src={src} alt={altText} className={styles.memberThumb} />
+        <Box className={styles.infoWrapper}>
+          <Heading className={styles.name}>{title}</Heading>
+          <Text className={styles.designation}>{designation}</Text>
+        </Box>
+        <Box className={styles.socialShare}>
+          {social.map((item) => (
+            <Link
+              key={item.id}
+              href={item.path}
+              target="_blank"
+              className={styles.socialLink}
+            >
+              {item.icon}
+            </Link>
+          ))}
+        </Box>
       </Box>
-      <Box className={styles.socialShare}>
-        {social.map((item) => (
-          <Link key={item.id} href={item.path} className={styles.socialLink}>
-            {item.icon}
-          </Link>
-        ))}
-      </Box>
-    </Box>
+    </Container>
   );
 };
 
