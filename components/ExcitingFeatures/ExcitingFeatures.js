60f3dcfeb067cedbbfc805eb5e73e11732bb5c43
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-18 23:50:58 +0530

    add Exciting features component

---
+++ components/ExcitingFeatures/ExcitingFeatures.js
@@ -0,0 +1,38 @@
+import { Box, Container, Image } from "@chakra-ui/react";
+import SectionHeading from "@/components/SectionHeading/SectionHeading";
+import AccordionComponent from "../Accordion/AccordionComponent";
+import styles from "./excitingFeatures.module.css";
+import data from "@/data/excitingFeatures.json";
+
+const ExcitingFeatures = () => {
+  return (
+    <Box as="section" variant="section.excitingFeatures">
+      <Container>
+        <Box className={styles.featureWrapper}>
+          <Box className={styles.illustrationWrapper}>
+            <Image
+              src={`/static/icons/feature.png`}
+              alt="feature"
+              className={styles.illustration}
+            />
+          </Box>
+          <Box className={styles.contentWrapper}>
+            <SectionHeading
+              className={styles.heading}
+              title="Meet our exciting features that make you wow"
+              description="Build an incredible workplace and grow your business with Gusto’s all-in-one platform with amazing contents."
+            />
+            <Box className={styles.accordionGroup}>
+              <AccordionComponent
+                className={styles.accordionStyle}
+                items={data}
+              />
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default ExcitingFeatures;
