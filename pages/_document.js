70f164f96b14cbb204d5de03399664bbc4abd971
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-20 01:04:57 +0530

    feat: add smooth scroll implementation

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html
+      lang="en"
+      className="scroll-smooth"
+      style={{ scrollBehavior: "smooth" }}
+    >
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
