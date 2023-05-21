c2fcf94363c5d5db1e4cd465ac0ff2db9e40e788
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-22 01:28:11 +0530

    add link prop with atttribute

--- components/Button/CustomButton.js
+++ components/Button/CustomButton.js
@@ -1,30 +1,35 @@
 import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 
-const CustomButton = ({ ...props }) => {
+const CustomButton = ({ path, ...props }) => {
+  const href = path || "";
   return (
-    <Button
-      variant="primary"
-      minHeight={["45px"]}
-      fontSize={["14px"]}
-      sx={{
-        marginTop: "0rem",
-        backgroundColor: "var(--primary-200)",
-        color: "var(--color-white)",
-        outline: 0,
-        borderRadius: ".3rem",
-        padding: "1rem 2.5rem",
-        fontFamily: "Poppins, sans-serif",
-        fontWeight: 800,
-        letterSpacing: ".05rem",
-        border: "none",
-        _hover: {
-          transition: ".3s ease-in-out",
-          backgroundColor: "var(--primary-300)",
-        },
-      }}
-      as="button"
-      {...props}
-    />
+    <Link href={href} passHref>
+      <Button
+        as="button"
+        // as={href ? "a" : "button"}
+        variant="primary"
+        minHeight={["45px"]}
+        fontSize={["14px"]}
+        sx={{
+          marginTop: "0rem",
+          backgroundColor: "var(--primary-200)",
+          color: "var(--color-white)",
+          outline: 0,
+          borderRadius: ".3rem",
+          padding: "1rem 2.5rem",
+          fontFamily: "Poppins, sans-serif",
+          fontWeight: 800,
+          letterSpacing: ".05rem",
+          border: "none",
+          _hover: {
+            transition: ".3s ease-in-out",
+            backgroundColor: "var(--primary-300)",
+          },
+        }}
+        {...props}
+      />
+    </Link>
   );
 };
 
