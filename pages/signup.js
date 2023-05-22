18fc2e0add96c8bc1744e0574579057a7f3df6d5
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-22 17:57:52 +0530

    add initial structure for custom toast messages

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { Box, FormControl, FormLabel, Input, Divider } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Divider,
+  useToast,
+} from "@chakra-ui/react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import styles from "@/styles/auth.module.css";
 import CustomButton from "@/components/Button/CustomButton";
 import { useRouter } from "next/router";
@@ -7,16 +15,54 @@ import Link from "next/link";
 
 const Signup = () => {
   const router = useRouter();
+  const toast = useToast();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [aadhaarCard, setAadhaarCard] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSignup = (e) => {
     e.preventDefault();
+
+    // Validate form inputs
+    if (
+      !firstName.trim() ||
+      !lastName.trim() ||
+      !aadhaarCard.trim() ||
+      !phoneNumber.trim() ||
+      !email.trim() ||
+      !password.trim()
+    ) {
+      toast({
+        title: "Error",
+        description: "Please fill in all required fields",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    // Additional validation logic here
+
     // Handle signup logic here
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const validateAadhaarCard = (value) => {
+    const aadhaarRegex = /^\d{12}$/;
+    return aadhaarRegex.test(value);
+  };
+
+  const validateName = (value) => {
+    const nameRegex = /^[A-Za-z]+$/;
+    return nameRegex.test(value);
   };
 
   return (
@@ -33,6 +79,7 @@ const Signup = () => {
               onChange={(e) => setFirstName(e.target.value)}
               className={styles.input}
               placeholder="Enter your first name"
+              isInvalid={firstName && !validateName(firstName)}
             />
           </FormControl>
 
@@ -44,6 +91,7 @@ const Signup = () => {
               onChange={(e) => setLastName(e.target.value)}
               className={styles.input}
               placeholder="Enter your last name"
+              isInvalid={lastName && !validateName(lastName)}
             />
           </FormControl>
 
@@ -55,17 +103,20 @@ const Signup = () => {
               onChange={(e) => setAadhaarCard(e.target.value)}
               className={styles.input}
               placeholder="Enter your Aadhaar card number"
+              maxLength={12}
+              isInvalid={aadhaarCard && !validateAadhaarCard(aadhaarCard)}
             />
           </FormControl>
 
           <FormControl id="phoneNumber" className={styles.formControl}>
             <FormLabel className={styles.formLabel}>Phone Number</FormLabel>
             <Input
-              type="text"
+              type="tel"
               value={phoneNumber}
               onChange={(e) => setPhoneNumber(e.target.value)}
               className={styles.input}
               placeholder="Enter your phone number"
+              isInvalid={phoneNumber && !phoneNumber.match(/^\d+$/)}
             />
           </FormControl>
 
@@ -77,18 +128,28 @@ const Signup = () => {
               onChange={(e) => setEmail(e.target.value)}
               className={styles.input}
               placeholder="Enter your email"
+              isInvalid={email && !email.includes("@")}
             />
           </FormControl>
 
           <FormControl id="password" className={styles.formControl}>
             <FormLabel className={styles.formLabel}>Password</FormLabel>
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className={styles.input}
-              placeholder="Enter your password"
-            />
+            <div className={styles.passwordInputContainer}>
+              <Input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className={styles.input}
+                placeholder="Enter your password"
+                isInvalid={password && password.length < 6}
+              />
+              <div
+                className={styles.passwordIcon}
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </div>
+            </div>
           </FormControl>
 
           <CustomButton type="submit" className={styles.btn}>
