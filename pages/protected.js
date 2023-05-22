edd2581de2adca3629d21be1c03da945bb688f56
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-23 00:06:50 +0530

    add loading state plus minor fix
    otherwise the content would be rendered for a brief moment
    before redirection happens

--- pages/protected.js
+++ pages/protected.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { useAuthContext } from "@/lib/firebase/context/AuthContext";
-import { useRouter, useEffect } from "next/router";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const ProtectedPage = () => {
   const { user } = useAuthContext();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!user) {
       // Redirect to the other page for unauthorized access
       router.push("/");
+    } else {
+      setIsLoading(false);
     }
   }, [user, router]);
+
+  if (isLoading) {
+    // Render a loading indicator or a blank screen while checking the authentication state
+    return <h1>Loading...</h1>;
+  }
 
   return <h1>Only logged-in users can view this page</h1>;
 };
