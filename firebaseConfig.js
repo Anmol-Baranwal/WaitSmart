709a5c34707ecf063652ecf482e01a966b8bc7ff
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-22 22:13:02 +0530

    add firebase config to set up firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,6 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -7,3 +10,9 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+// Initialize Firebase
+let firebase_app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export default firebase_app;
