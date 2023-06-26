539fb795a851bb8f693eb0345e9bc0ccd3b9443e
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-06-26 17:47:38 +0530

    feat: fetch and display data from firestore

--- pages/doctor/[doctorId].js
+++ pages/doctor/[doctorId].js
@@ -1,9 +1,38 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import getData from "@/lib/firebase/firestore/getData"; // Import the getData function
 
 const Doctor = () => {
   const router = useRouter();
+  const [userData, setUserData] = useState(null);
 
-  return <div>Doctor Dynamic page {router.query.doctorId} </div>;
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const { result, error } = await getData("users", router.query.doctorId);
+      if (result) {
+        setUserData(result.data());
+      } else {
+        console.log(error);
+      }
+    };
+
+    fetchUserData();
+  }, [router.query.doctorId]);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {/* <p>Doctor Dynamic page {router.query.doctorId}</p> */}
+      <p>First Name: {userData.firstName}</p>
+      <p>Last Name: {userData.lastName}</p>
+      <p>Email: {userData.email}</p>
+      <p>Phone Number: {userData.phoneNumber}</p>
+      <p>Aadhaar Card: {userData.aadhaarCard}</p>
+    </div>
+  );
 };
 
 export default Doctor;
