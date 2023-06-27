60deb45d35b0d48826650be7c508a69c6d8c1a13
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-06-27 19:17:28 +0530

    feat: use backend api for the request
    to add patient record in the database

--- pages/appointment.js
+++ pages/appointment.js
@@ -55,58 +55,34 @@ const Appointment = () => {
     if (e) e.preventDefault();
 
     try {
-      const doctorRef = collection(db, "doctors");
-      const doctorQuery = query(doctorRef, where("name", "==", selectedDoctor));
-      const doctorSnapshot = await getDocs(doctorQuery);
+      const response = await fetch("/api/createDoctorAppointment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          contactNumber,
+          selectedDoctor,
+          city,
+        }),
+      });
 
-      if (doctorSnapshot.empty) {
-        // Create a new doctor document in the "doctors" collection
-        const newDoctorDoc = await addDoc(doctorRef, {
-          name: selectedDoctor,
-        });
-        const doctorId = newDoctorDoc.id;
-
-        // Create a new document in the "patients" subcollection within the doctor's document
-        const newPatientDoc = await addDoc(
-          collection(db, "doctors", doctorId, "patients"),
-          {
-            firstName,
-            lastName,
-            contactNumber,
-            city,
-          }
-        );
-        const patientId = newPatientDoc.id;
+      if (response.ok) {
+        const data = await response.json();
+        const { doctorId, patientId } = data;
 
         console.log(
           "Appointment created successfully with doctor ID:",
           doctorId
         );
         console.log("Patient data added successfully with ID:", patientId);
+
+        router.push("/appointment");
       } else {
-        // Use the existing doctor ID
-        const doctorId = doctorSnapshot.docs[0].id;
-
-        // Create a new document in the "patients" subcollection within the existing doctor's document
-        const newPatientDoc = await addDoc(
-          collection(db, "doctors", doctorId, "patients"),
-          {
-            firstName,
-            lastName,
-            contactNumber,
-            city,
-          }
-        );
-        const patientId = newPatientDoc.id;
-
-        console.log(
-          "Appointment created successfully with existing doctor ID:",
-          doctorId
-        );
-        console.log("Patient data added successfully with ID:", patientId);
+        console.log("Failed to create appointment");
       }
-
-      router.push("/appointment");
     } catch (error) {
       console.log(error);
     }
